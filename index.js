3dfe9d6980af2072e740b264e763f4f9b459a8ca
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-01-27 18:33:07 +0100

    Add ludwig-api

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./lib/config/config');
+var ludwigApi = require('ludwig-api');
 
 // Setup mongoose
 require('./lib/config/mongoose')(mongoose, config);
 
 // Setup Express
 var app = express();
+
+// Mount Ludwig API
+var Situation = mongoose.model('Situation');
+
+app.use(ludwigApi({
+
+    mongoose: mongoose,
+
+    simulate: function (acceptanceTest, done) {
+        Situation.findById(acceptanceTest.scenario.situationId).exec(function (err, situation) {
+            if (err) return done(err);
+            if (!situation) return done(new Error('Situation not found'));
+            situation.simulate(done);
+        });
+    },
+
+    onCreate: function (acceptanceTest, done) {
+        var situationId = acceptanceTest.scenario.situationId;
+
+        Situation.findById(situationId).exec(function (err, situation) {
+            if (err) return done(err);
+            if (!situation) return done(new Error('Situation not found'));
+            situation.set('status', 'test');
+            situation.save(done);
+        });
+    }
+
+}));
 
 // Setup api
 require('./lib/config/api')(app);
