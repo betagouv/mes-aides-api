eb89fa9304db0a08a126ae7289c16f6bb4fdc743
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-01-27 17:51:52 +0100

    Extract ludwig-api

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var passport = require('passport');
 var config = require('./lib/config/config');
 
 // Setup mongoose
 require('./lib/config/mongoose')(mongoose, config);
 
-// Setup Passport
-require('./lib/config/passport')(passport);
-
 // Setup Express
 var app = express();
 
 // Setup api
-require('./lib/config/api')(app, passport, config);
+require('./lib/config/api')(app);
 
 module.exports = app;
