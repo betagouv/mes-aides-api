d7e4a300fd65a294650a8e4f65267279568a1c70
Florian Pagnoux <Florian.Pagnoux@gmail.com>
2015-10-12 11:52:14 +0200

    Add migration script for rsa isolement

---
+++ migrations/rsa-isolement.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var es = require('event-stream');
+var config = require('../lib/config/config');
+var _ = require('lodash');
+
+require('../lib/config/mongoose')(mongoose, config);
+
+var Situation = mongoose.model('Situation');
+
+Situation.find({ status: 'test' }).stream()
+    .pipe(es.map(function (situation, done) {
+
+        var isSituationUpdated = false;
+        hasConjoint = _.find(situation.individus, { role: 'conjoint' });
+        hasEnfant = _.find(situation.individus, { role: 'enfant' });
+
+        if (hasEnfant && ! hasConjoint) {
+            situation.individus[0].isolementRecent = true;
+            isSituationUpdated = true;
+        }
+
+        situation.save(function (err) {
+            if (err) {
+                console.log('Cannot save migrated situation %s', situation.id);
+                console.trace(err);
+            }
+            else if (isSituationUpdated)
+                console.log('Situation migrée');
+            done();
+        });
+    }))
+    .on('end', function() {
+        console.log('Terminé');
+        process.exit();
+    })
+    .on('error', function(err) {
+        console.trace(err);
+        process.exit();
+    })
+    .resume();
