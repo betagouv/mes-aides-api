cacf197e720fdfc98686856bdf13ab047c864295
Thomas Guillet <guillet.thomas@gmail.com>
2017-04-24 17:39:39 +0200

    Rely on migrateAllSituations API to migration situations to v8

--- migrations/v7.3.2_8.0.0.js
+++ migrations/v7.3.2_8.0.0.js
@@ -1,48 +1,23 @@
-var mongoose = require('mongoose');
 var _ = require('lodash');
-var es = require('event-stream');
-var config = require('../lib/config/config');
-
-require('../lib/config/mongoose')(mongoose, config);
-
-var Situation = mongoose.model('Situation');
+var migrateAllSituations = require('./migrations').migrateAllSituations;
 
 var mapping = {
-    'primeActivite': 'ppa',
+    'clca': 'paje_clca',
     'prepare': 'paje_prepare',
-    'clca': 'paje_clca'
+    'primeActivite': 'ppa'
 };
 
 var keys = _.keys(mapping);
 
-Situation.find({ 'individus.ressources.type': { '$in': _.keys(mapping) } }).stream()
-    .pipe(es.map(function (situation, done) {
-        var isSituationUpdated = false;
-        situation.individus.forEach(function (individu) {
-            individu.ressources.forEach(function(ressource) {
-                if (_.contains(keys, ressource.type)) {
-                    ressource.type = mapping[ressource.type];
-                    isSituationUpdated = true;
-                }
-            });
-        });
-        situation.save(function (err) {
-            if (err) {
-                console.log('Cannot save migrated situation %s', situation.id);
-                console.trace(err);
+migrateAllSituations(function(situation) {
+    var isSituationUpdated = false;
+    situation.individus.forEach(function (individu) {
+        individu.ressources.forEach(function(ressource) {
+            if (_.contains(keys, ressource.type)) {
+                ressource.type = mapping[ressource.type];
+                isSituationUpdated = true;
             }
-            else if (isSituationUpdated) {
-                console.log('Situation migrée');
-            }
-            done();
         });
-    }))
-    .on('end', function() {
-        console.log('Terminé');
-        process.exit();
-    })
-    .on('error', function(err) {
-        console.trace(err);
-        process.exit();
-    })
-    .resume();
+    });
+    return isSituationUpdated;
+});
