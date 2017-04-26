82f18f3d71b2502729a19b26c01b921e622d2582
Thomas Guillet <guillet.thomas@gmail.com>
2017-04-26 15:13:00 +0200

    Rename indJourMaladie to indemnites_journalieres_maladie

--- migrations/v7.3.3_8.0.0.js
+++ migrations/v7.3.3_8.0.0.js
@@ -15,6 +15,7 @@ var ressourceMapping = {
     'gainsExceptionnels': 'gains_exceptionnels',
     'indChomagePartiel': 'indemnites_chomage_partiel',
     'indJourAccidentDuTravail': 'indemnites_journalieres_accident_travail',
+    'indJourMaladie': 'indemnites_journalieres_maladie',
     'indJourMaladieProf': 'indemnites_journalieres_maladie_professionnelle',
     'clca': 'paje_clca',
     'prepare': 'paje_prepare',
